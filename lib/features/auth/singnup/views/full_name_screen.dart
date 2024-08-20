cc575cda9f2bd8b226be05578308345e3755e7a2
ys1113457623 <ys1113457623@gmail.com>
2024-08-20 23:42:20 +0530

    feat: signup views

---
+++ lib/features/auth/singnup/views/full_name_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:clay_containers/clay_containers.dart';
+
+class FullNamePage extends StatelessWidget {
+  final VoidCallback onNext;
+
+  FullNamePage({required this.onNext});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    Color baseColor = Color(0xFFE3EDF7);
+
+    return Container(
+      height: screenHeight,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Enter your full name', style: TextStyle(fontSize: 24)),
+                  SizedBox(height: 16), // Space between text and input field
+                  ClayContainer(
+                      color: baseColor,
+                      spread: 3,
+                      emboss: true,
+                      width:
+                          double.infinity, // Make the ClayContainer full width
+                      borderRadius: 12,
+                      child: Center(
+                          child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 12,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none, // Remove default border
+
+                            hintText: 'Enter full name',
+                          ),
+                        ),
+                      ))),
+                ],
+              ),
+            ),
+            Container(
+              height: 80,
+              width: 80,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.red, // Border color
+                  width: 2, // Border width
+                ),
+                borderRadius: BorderRadius.circular(
+                    50), // Match the ClayContainer border radius
+              ),
+              child: Center(
+                // Center the ClayContainer within the Container
+                child: ClayContainer(
+                  spread: 0,
+                  emboss: true,
+                  color: baseColor, // Base color for ClayContainer
+                  height: 70,
+                  width: 70,
+                  borderRadius: 50,
+                  child: Center(
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_forward, color: Colors.black45),
+                      onPressed:
+                          onNext, // Call the onNext function when pressed
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/singnup/views/phonenumber_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:clay_containers/clay_containers.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+
+class PhoneNumberPage extends StatelessWidget {
+  final VoidCallback onNext;
+
+  PhoneNumberPage({required this.onNext});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    Color baseColor = Color(0xFFE3EDF7);
+
+    return Container(
+      height: screenHeight,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Enter your phone number',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  SizedBox(
+                      height: 16), // Add space between text and input field
+                  ClayContainer(
+                      color: baseColor,
+                      spread: 3,
+                      emboss: true,
+                      width:
+                          double.infinity, // Make the ClayContainer full width
+                      borderRadius: 12,
+                      child: Center(
+                        child: IntlPhoneField(
+                          disableLengthCheck: true,
+                          showCountryFlag: false,
+                          textAlignVertical: TextAlignVertical.center,
+                          decoration: InputDecoration(
+                            border: InputBorder.none, // Remove default border
+                            hintText: 'Phone Number',
+                            hintStyle: TextStyle(color: Colors.black54),
+                          ),
+                          initialCountryCode: 'IN',
+                          onChanged: (phone) {
+                            print(phone.completeNumber);
+                          },
+                        ),
+                      )),
+                ],
+              ),
+            ),
+            Container(
+              height: 80,
+              width: 80,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.red, // Border color
+                  width: 2, // Border width
+                ),
+                borderRadius: BorderRadius.circular(
+                    50), // Match the ClayContainer border radius
+              ),
+              child: Center(
+                // Center the ClayContainer within the Container
+                child: ClayContainer(
+                  spread: 0,
+                  emboss: true,
+                  color: baseColor, // Base color for ClayContainer
+                  height: 70,
+                  width: 70,
+                  borderRadius: 50,
+                  child: Center(
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_forward, color: Colors.black54),
+                      onPressed:
+                          onNext, // Call the onNext function when pressed
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
