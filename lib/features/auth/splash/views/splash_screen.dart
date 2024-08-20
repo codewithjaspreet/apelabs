888b73922ee202aaee00f9d5ececa8fddfe93b02
codewithjaspreet <jaspreetsodhi02@gmail.com>
2024-08-20 20:31:18 +0530

    splash-updated

--- lib/features/auth/splash/views/splash_screen.dart
+++ lib/features/auth/splash/views/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:apelabs/features/auth/onboarding/views/onboarding_screen.dart';
+import 'package:apelabs/main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
@@ -11,9 +13,34 @@ class SplashScreen extends StatelessWidget {
       Get.to(const OnboardingScreen()); // Use Get.offNamed to navigate without the option to return to SplashScreen
     });
     return  Scaffold(
-      
+
       body: Center(
-        child: Image.asset('assets/images/splash_image.png'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Opacity(
+                  opacity: 0.3,
+                  child: Image.asset(
+                    'assets/images/splash_image.png',
+                    width: 400.w,
+                    height: 400.h,
+                  ),
+                ),
+                // Logo on top
+                Center(
+                  child: Image.asset(
+                    'assets/images/app_logo.png',
+                    width: 300.h,
+                    height: 300.w,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
 
 
